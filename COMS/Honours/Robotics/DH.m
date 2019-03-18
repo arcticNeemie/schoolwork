fde9cb03f2c877aa7198e4554d0ce7bbfb71cc0c
tamlinlove <tamlinlollislove@gmail.com>
2019-03-18 07:36:32 +0200

    Minor Robotics matlab stuff

--- COMS/Honours/Robotics/DH.m
+++ COMS/Honours/Robotics/DH.m
@@ -1,10 +1,10 @@
 function A = DH(a,alpha,d,theta)
      %
-    %Input: a, alpha, d, theta - all scalars (angles in degrees)
+    %Input: a, alpha, d, theta - all scalars (angles in radians)
     %Output: A, the corresponding Denavit-Hartenberg transformation matrix
     %
-    theta = theta*pi/180;
-    alpha = alpha*pi/180;
+    %theta = theta*pi/180;
+    %alpha = alpha*pi/180;
     A = [cos(theta),-sin(theta)*cos(alpha),sin(theta)*sin(alpha),a*cos(theta);
         sin(theta),cos(theta)*cos(alpha),-cos(theta)*sin(alpha),a*sin(theta);
         0,sin(alpha),cos(alpha),d;
